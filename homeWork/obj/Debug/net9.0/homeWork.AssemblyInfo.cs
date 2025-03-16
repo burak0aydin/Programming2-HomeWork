a451404cddd85a5b268417068139d899e90100bf
Burak Aydin <aslan.burak_@hotmail.com>
2025-03-16 12:56:22 +0300

    Çakışmalar çözüldü

--- homeWork/obj/Debug/net9.0/homeWork.AssemblyInfo.cs
+++ homeWork/obj/Debug/net9.0/homeWork.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("homeWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cf2d6515fcfd522612f4161452ba431e1cb69ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("homeWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("homeWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
